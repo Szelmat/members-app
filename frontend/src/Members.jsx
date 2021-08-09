b0d73a76adcdd1899a31666e1082e3338fd2fc35
Szelmat <szelessm@gmail.com>
2021-08-09 11:54:53 +0200

    implement delete method

--- frontend/src/Members.jsx
+++ frontend/src/Members.jsx
@@ -15,23 +15,30 @@ export function Members(props) {
     fetchMembers();
   }, [members]);
 
+  function deleteMember(id) {
+    fetch('http://127.0.0.1:8000/api/' + id, {
+      method: 'DELETE',
+    });
+  }
+
   return (
     <div>
       <h1 id="membersTitle">Tagok</h1>
       <ul id="memberList">
         {members.map((member) => {
           return (
-            <li>
+            <li key={member.name}>
               <div className="memberCard">
                 <div className="memberButtons">
                 <button className="editMember">
                   <IoPencil />
                 </button>
                 
-                <button className="deleteMember">
+                <button className="deleteMember" onClick={() => deleteMember(member.id)}>
                   <IoPersonRemoveOutline />
                 </button>
                 </div>
+                <span className="memberId">{member.id}</span>
                 <h2>{member.name}</h2>
                 <h4>{member.clubName}</h4>
                 <h4>{member.birth.replaceAll('-', '.') + '.'}</h4>
